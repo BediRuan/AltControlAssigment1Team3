8c8bf8980f69330d0bf870d501e182767b915038
kflores33 <159832582+kflores33@users.noreply.github.com>
2025-10-18 01:31:19 -0700

    Basic game loop implemented

--- Assets/KayFlores/Scripts/GameManager.cs
+++ Assets/KayFlores/Scripts/GameManager.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -10,10 +9,88 @@ public class GameManager : MonoBehaviour
         else Destroy(this);
     }
 
+    public HairSection CurrentCustomerOrder { get; private set; } = null;
+
+    float hairCurlAccuracy = 0;
+    public float HairCurlMinAccuracy = 80f; // minimum % to be considered accurate
+    public float HairCurlAccuracyThreshold = 10f;
+
+    float hairCutAccuracy = 0;
+    public float HairCutMinAccuracy = 80f; // minimum % to be considered accurate
+    public float HairCutAccuracyThreshold = 1f;
+
     // game structure is basically:
     // get customer order
     // display order type & allow player to go in whatever order they want
     // swap between modes (cut, curl) 
 
-    //this will handle game loop stuff + spawning
+    private void Start()
+    {
+        GenerateNewOrder();
+    }
+
+    private void Update()
+    {
+        if (CurrentCustomerOrder == null)
+        {
+            GenerateNewOrder();
+        }
+    }
+
+    public void GenerateNewOrder()
+    {
+        CurrentCustomerOrder = new HairSection() // applies to both sides of hair
+        {
+            curlAmount = Random.Range(0f, 1f) > 0.4f ? Random.Range(20f, 70f) : 0f, // 60% chance of having curls (of any amount)
+            currentLength = Random.Range(1, 4), // length between 1 and 3 (default is 4)
+        };
+        Debug.Log($"New Customer Order: Curl Percentage is {CurrentCustomerOrder.curlAmount} | Cut Length is {CurrentCustomerOrder.currentLength}");
+    }
+
+    private void CompareOrderWithSection(HairSection playerHair)
+    {
+        hairCurlAccuracy = 100f - (Mathf.Abs(playerHair.curlAmount - CurrentCustomerOrder.curlAmount) / HairCurlAccuracyThreshold) * 100f;
+        hairCutAccuracy = 100f - (Mathf.Abs(playerHair.currentLength - CurrentCustomerOrder.currentLength) / HairCutAccuracyThreshold) * 100f;
+
+        // compare playerHair with CurrentCustomerOrder
+        if (playerHair.curlAmount >= CurrentCustomerOrder.curlAmount - HairCurlAccuracyThreshold &&
+            playerHair.curlAmount <= CurrentCustomerOrder.curlAmount + HairCurlAccuracyThreshold)
+        {
+            hairCurlAccuracy = 100f;
+        }
+        if (playerHair.currentLength == CurrentCustomerOrder.currentLength)
+        {
+            hairCutAccuracy = 100f;
+        }
+
+        if (hairCurlAccuracy < 0) hairCurlAccuracy = 0;
+        if (hairCutAccuracy < 0) hairCutAccuracy = 0;
+
+        Debug.Log($"{playerHair} Curl Accuracy: {hairCurlAccuracy}% | {playerHair} Cut Accuracy: {hairCutAccuracy}%");
+    }
+
+    public void CalculateResult(HairSection L, HairSection R)
+    {
+        CompareOrderWithSection(L);
+        float leftCurlAccuracy = hairCurlAccuracy;
+        float leftCutAccuracy = hairCutAccuracy;
+
+        CompareOrderWithSection(R);
+        float rightCurlAccuracy = hairCurlAccuracy;
+        float rightCutAccuracy = hairCutAccuracy;
+
+        hairCurlAccuracy = (leftCurlAccuracy + rightCurlAccuracy) / 2f; // average both sides (curl)
+        hairCutAccuracy = (leftCutAccuracy + rightCutAccuracy) / 2f; // average both sides (cut)
+
+        if (hairCurlAccuracy >= HairCurlMinAccuracy && hairCutAccuracy >= HairCutMinAccuracy)
+        {
+            Debug.Log("Customer is satisfied!");
+        }
+        else
+        {
+            Debug.Log("Customer is not satisfied.");
+        }
+
+        CurrentCustomerOrder = null; // reset order
+    }
 }
--- Assets/KayFlores/Scripts/PlayerInputManager.cs
+++ Assets/KayFlores/Scripts/PlayerInputManager.cs
@@ -4,15 +4,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine.InputSystem;
 
-
-//[System.Serializable]
-//public class HairPiece
-//{
-//    public KeyCode input;
-//    public string strandSection; // left or right (which strand this piece belongs to) (might be redundant
-//    public int length; // 1, 2, or 3 (3 is longest)
-//    //public float curlAmount = 0;
-//}
 [System.Serializable]
 public class HairSection
 {
@@ -34,26 +25,6 @@ public class PlayerInputManager : MonoBehaviour
 
     public enum Tools { None, Scissors, CurlingIron }
     public Tools currentTool = Tools.None;
-
-    //public Dictionary<int, HairPiece> leftPart { get; private set; } = new Dictionary<int, HairPiece>();
-
-    //public Dictionary<int, HairPiece> rightPart { get; private set; } = new Dictionary<int, HairPiece>();
-
-    //void PopulateStrand(Dictionary<int, HairPiece> dict, string side) // defaults
-    //{
-    //    if (side == "left")
-    //    {        
-    //        dict.Add(1, new HairPiece { input = KeyCode.Q, strandSection = side, length =  1});
-    //        dict.Add(2, new HairPiece { input = KeyCode.W, strandSection = side, length = 2 });
-    //        dict.Add(3, new HairPiece { input = KeyCode.E, strandSection = side, length = 3 });
-    //    }
-    //    else if (side == "right") 
-    //    {
-    //        dict.Add(1, new HairPiece { input = KeyCode.R, strandSection = side, length = 1 });
-    //        dict.Add(2, new HairPiece { input = KeyCode.T, strandSection = side, length = 2 });
-    //        dict.Add(3, new HairPiece { input = KeyCode.Y, strandSection = side, length = 3 });
-    //    }
-    //}
 
     public HairSection currentSectionL { get; private set; }
     public HairSection currentSectionR { get; private set; }
@@ -99,6 +70,11 @@ public class PlayerInputManager : MonoBehaviour
                 UpdateNone();
                 break;
         }
+
+        if (keyboard.enterKey.wasPressedThisFrame)
+        {
+            GameManager.instance.CalculateResult(currentSectionL, currentSectionR);
+        }
     }
 
     void UpdateNone()
@@ -108,7 +84,6 @@ public class PlayerInputManager : MonoBehaviour
             currentTool = Tools.CurlingIron;
         }
     }
-
     public void UpdateScissors()
     {
         if (keyboard.sKey.wasPressedThisFrame)
